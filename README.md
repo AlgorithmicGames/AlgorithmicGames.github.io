c90bd0252dd5c433dac943db53c215405eb999fa
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-08-17 14:35:59 +0200

    Added link to communication.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AI-Tournaments
-The website.
+AI Tournaments (.io) is still in an early prototype stage. If you want to participate, make a Fork of a `[ARENA]-AI-Tournament-Participant` repository and analyze how other forks have solved the arena until better documentation has been written.
+
+Click here to [join the community](https://github.com/AI-Tournaments/AI-Tournaments/issues/1).
--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function a(){
 	calcSize();
 	window.onresize = calcSize;
 	play();
-	addMessage('Wellcome servant!','Make your Master proud by participate in our arenas based upon different kinds of AI and algorithm driven games.\n<span style="color:var(--secondary-background-color)">- Overlord servant</span>', true, '397px');
-	addMessage('Work in progress', 'Sorry, but Overlord has not publicly open the arenas yet so there is nothing to see here at the moment.\nBut dig around or get back here soon™ because awesome stuff are coming!\nRead more about aitournaments.io over at <a href="https://github.com/AI-Tournaments" target="_blank">GitHub</a>.\n<span style="color:var(--secondary-background-color)">- Overlord servant</span>', true, '50%');
+	openWindow('Wellcome servant!','Make your Master proud by participate in our arenas based upon different kinds of AI and algorithm driven games.\n<span style="color:var(--secondary-background-color)">- Overlord servant</span>', true, '397px');
+	openWindow('Work in progress', 'Sorry, but Overlord has not publicly open the arenas yet so there is nothing to see here at the moment.\nBut dig around or get back here soon™ because awesome stuff are coming!\nRead more about aitournaments.io over at <a href="https://github.com/AI-Tournaments/AI-Tournaments" target="_blank">GitHub</a> or join the discussion <a href="https://github.com/AI-Tournaments/AI-Tournaments/issues/1" target="_blank">here</a>.\n<span style="color:var(--secondary-background-color)">- Overlord servant</span>', true, '50%');
 	function makeDragable(trigger, dragable=trigger){
 		let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 		trigger.onmousedown = dragMouseDown;
@@ -36,7 +36,7 @@ function a(){
 			document.onmousemove = null;
 		}
 	}
-	function addMessage(header='', message='', center=false, maxWidth){
+	function openWindow(header='', message='', center=false, maxWidth){
 		let windowWrapper = document.createElement('div');
 		if(maxWidth !== undefined){
 			windowWrapper.style.maxWidth = maxWidth;
