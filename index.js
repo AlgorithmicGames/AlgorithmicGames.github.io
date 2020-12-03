f06f20027c093705a76e00af4d9ba6f15889bcbe
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2020-12-03 23:01:35 +0100

    Added crash warning.

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ function a(){
 	if(Backend.isOverride()){
 		openWindow('⚠️Attention: Backend override⚠️','Backend is currently set to: '+Backend.getBackend()+'<br><button onclick="localStorage.removeItem(\'backend\'); location.reload();">Reset</button>',false);
 	}
+	if(navigator.userAgent.indexOf("Firefox") === -1){
+		openWindow('Known problem: browser crash','AI-Tournaments can crash in some browsers when running matches in the client, Chrome is for example calling it "Oh, snap! STATUS_ACCESS_VIOLATION". If you are facing this problem, try using Firefox until it is sorted. Read more <a href="https://github.com/AI-Tournaments/AI-Tournaments/issues/2" target="_blank">here</a>.',false,'424px');
+	}
 	fetch('https://raw.githubusercontent.com/AI-Tournaments/AI-Tournaments/master/README.md').then(response => response.text()).then(readme => {
 		let why = readme.replace(/.+?(?=## Why Source Available?)/s, '').replace(/.*\n/,'');
 		fetch('https://gitlab.com/api/v4/markdown',{method: 'POST', body: JSON.stringify({text: why}),
