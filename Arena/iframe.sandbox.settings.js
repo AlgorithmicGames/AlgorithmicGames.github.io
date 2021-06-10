03935076b75de588ada0ce4fcefa9eb7578596fd
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-06-10 23:37:39 +0200

    HTML5.

--- Arena/iframe.sandbox.settings.js
+++ Arena/iframe.sandbox.settings.js
@@ -60,7 +60,7 @@ function a(){
 				let iframedMessage = document.createElement('iframe');
 				iframedMessage.sandbox = '';
 				iframedMessage.classList.add('message');
-				iframedMessage.srcdoc = '<html><head><link rel="stylesheet" href="../defaults.css"><script></script></head><body>'+comment.message+'</body></html>';
+				iframedMessage.srcdoc = '<!DOCTYPE html><html><head><link rel="stylesheet" href="../defaults.css"><script></script></head><body>'+comment.message+'</body></html>';
 				if(comment.height !== undefined){
 					iframedMessage.height = comment.height;
 				}
