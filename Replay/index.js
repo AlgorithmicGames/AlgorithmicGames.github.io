8bc5ae083fd584a490273ede34048a32639d81e8
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-08-09 09:09:31 +0200

    Give replay full ArenaResult.

--- Replay/index.js
+++ Replay/index.js
@@ -20,7 +20,7 @@ function a(){
 	let _element_previousReplaysController = document.getElementById('previous-replays-controller');
 	let _parent = null;
 	let _replayOptionsPromise_resolve;
-	let _replayOptionsPromise = new Promise(resolve=>_replayOptionsPromise_resolve=resolve);
+	let _replayOptionsPromise = new Promise(resolve=>_replayOptionsPromise_resolve=resolve); // Not referenced.
 	let _editor = new JSONEditor(_element_editor, {'modes': ['code', 'view'], 'name': 'Replay', 'onChange': onChange, 'onValidate': onValidate});
 	setTimeout(()=>{
 		if(_editor.getText() === '{}'){
@@ -64,10 +64,10 @@ function a(){
 				message: 'Property "defaultReplay" is not a URL.'
 			});
 		}
-		if(json.body === undefined || typeof json.body.data !== 'object'){
+		if(json.body === undefined || typeof json.body.matchLogs !== 'object'){
 			errors.push({
 				path: ['body'],
-				message: 'Property "data" is missing or not a array.'
+				message: 'Property "matchLogs" is missing or not a array.'
 			});
 		}
 		return errors;
@@ -75,55 +75,12 @@ function a(){
 	function onChange(){
 		_editor.validate().then(errors => {
 			let containsError = 0 < errors.length;
-			[...document.getElementsByClassName('select-match-button')].forEach(input=>{
-				input.parentElement.removeChild(input);
-			});
 			_element_btnLock.disabled = containsError;
 			document.getElementById('invalid-input').classList[containsError ? 'remove' : 'add']('hidden');
 			if(containsError){
 				_element_editor.classList.remove('hidden');
 			}else{
 				_replayData = _editor.get();
-				_replayData.body.data.forEach((matchLog, index) => {
-					let input = document.createElement('input');
-					input.type = 'button';
-					input.value = 'Match ' + (index+1);
-					let aborted = matchLog === null;
-					if(aborted){
-						input.value += ' (aborted)';
-					}
-					input.dataset.aborted = aborted;
-					input.disabled = true;
-					input.classList.add('select-match-button');
-					input.classList.add('sticky');
-					function onClick(mouseEvent){
-						for(const matchButton of document.getElementsByClassName('select-match-button')){
-							if(matchButton !== input && matchButton.dataset.aborted !== 'true'){
-								matchButton.disabled = false;
-							}
-						}
-						for(const element of _element_control.children){
-							if(!element.classList.contains('sticky')){
-								element.classList.add('hidden');
-							}
-						}
-						_element_iframe.dataset.matchLog = JSON.stringify(matchLog);
-						_element_iframe.src = _element_viewOptions.selectedOptions[0].value;
-						document.getElementById('open-replay-in-new-tab').addEventListener('click', ()=>{
-							let win = window.open(_element_viewOptions.selectedOptions[0].value);
-							setTimeout(()=>{
-								win.postMessage({type: 'Match-Log', matchLog: matchLog}, '*');
-							}, 1000);
-						});
-						input.disabled = true;
-					}
-					input.addEventListener('click', onClick);
-					_element_control.appendChild(input);
-					if(_replayData.body.data.length === 1){
-						input.classList.add('hidden');
-						_replayOptionsPromise.then(onClick);
-					}
-				});
 			}
 			if(_autoStart){
 				_autoStart = false;
@@ -162,14 +119,13 @@ function a(){
 						let optgroup = document.createElement('optgroup');
 						optgroup.label = groupedReplay.name;
 						groupedReplay.list.forEach(storedReplay => {
-							let participants = ['Aborted'];
+							debugger
+							let participants = storedReplay.data.body.teams.map(t => t.members.join(', '));
 							let scores = [];
-							let hasScore = storedReplay.data.body.data.filter(d => d.score);
+							let hasScore = storedReplay.data.body.matchLogs.filter(d => d.scores);
 							if(hasScore.length){
-								hasScore = hasScore[0].score;
-								hasScore.forEach((item, index) => participants[index] = item.members.map(member => member.name).join(', '));
-								storedReplay.data.body.data.forEach(match=>{
-									scores.push(match.score.map(s => s.score).join('-'));
+								storedReplay.data.body.matchLogs.forEach(match=>{
+									scores.push(match.scores.map(s => s.score).join('-'));
 								})
 							}
 							let option = document.createElement('option');
@@ -193,8 +149,8 @@ function a(){
 					_element_btnClearStoredReplays.disabled = storedReplays.length === 0;
 				});
 			}
-			refreshStoredReplays();
 			document.getElementById('load-previous-replay').addEventListener('click', ()=>{
+				refreshStoredReplays();
 				_element_control.classList.add('hidden');
 				_element_previousReplayContainer.classList.remove('hidden');
 			});
@@ -279,7 +235,7 @@ function a(){
 						break;
 					case 'ReplayHelper-Initiated':
 						messageEvent.source.postMessage({type: 'Init-Fetch-Replay-Height'}, '*');
-						messageEvent.source.postMessage({type: 'Match-Log', matchLog: JSON.parse(_element_iframe.dataset.matchLog)}, '*');
+						messageEvent.source.postMessage({type: 'Arena-Result', arenaResult: JSON.parse(_element_iframe.dataset.arenaResult)}, '*');
 						setTimeout(()=>{
 							if(_element_iframe.classList.contains('hidden')){
 								_element_iframe_failToLoad.classList.remove('hidden');
@@ -315,9 +271,6 @@ function a(){
 				_element_btnLock.disabled = true;
 				_editor.setMode('view');
 				addReplayToStorage(_editor.get());
-				for(const input of document.getElementsByClassName('select-match-button')){
-					input.disabled = input.dataset.aborted === 'true';
-				}
 				_replayOptionsPromise_resolve(GitHubApi.fetch('search/repositories?q=topic:AI-Tournaments+topic:AI-Tournaments-Replay+topic:'+_replayData.body.arena.full_name.replace('/','--')).then(response => response.json()).then(response => {
 					document.getElementById('default-option').value = _replayData.header !== undefined && _replayData.header.defaultReplay !== undefined && _replayData.header.defaultReplay !== '' ? _replayData.header.defaultReplay : 'https://ai-tournaments.github.io/'+_replayData.body.arena.full_name.split('/')[1].replace('Arena','Replay')+'/';
 					response.items.forEach(repo => {
@@ -338,6 +291,20 @@ function a(){
 						return 0;
 					});
 					_element_viewOptions.classList.remove('hidden');
+					// Load replay.
+					for(const element of _element_control.children){
+						if(!element.classList.contains('sticky')){
+							element.classList.add('hidden');
+						}
+					}
+					_element_iframe.dataset.arenaResult = JSON.stringify(_replayData.body);
+					_element_iframe.src = _element_viewOptions.selectedOptions[0].value;
+					document.getElementById('open-replay-in-new-tab').addEventListener('click', ()=>{
+						let win = window.open(_element_viewOptions.selectedOptions[0].value);
+						setTimeout(()=>{
+							win.postMessage({type: 'Arena-Result', arenaResult: JSON.parse(_element_iframe.dataset.arenaResult)}, '*');
+						}, 1000);
+					});
 				}));
 			};
 		};
