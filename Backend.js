0f3df27aff2bd500430259ad357105e795bc15e7
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2021-12-29 20:57:03 +0100

    Update Backend.js

--- Backend.js
+++ Backend.js
@@ -4,7 +4,7 @@ class Backend{
 		return fetch(new Request(Backend.getBackend()+'?'+module), {method:'POST',body:JSON.stringify(data)}).then(response => response.json());
 	}
 	static getBackend(){
-		return localStorage.getItem('backend') ?? 'https://backend.aitournaments.io';
+		return localStorage.getItem('backend') ?? 'https://backend.ai-tournaments.io';
 	}
 	static isOverride(){
 		return localStorage.getItem('backend') !== null;
