a46383340316dc68e41b4aef0961643d1ac5862a
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-01-13 23:07:55 +0100

    Display editor on auto-load.

--- Replay/index.js
+++ Replay/index.js
@@ -48,6 +48,7 @@ function a(){
 		IndexedDBOperation.do({operation: 'getStoredReplayData', data: replayID}).then(replayData => {
 			_editor.setMode('view');
 			_editor.setText(JSON.stringify(replayData));
+			_element_editor.classList.remove('hidden');
 			onChange();
 		});
 	}
