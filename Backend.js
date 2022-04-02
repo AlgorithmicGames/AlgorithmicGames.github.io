6100df68d38b61d796607c151f1d506edcae5de0
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-04-03 00:35:25 +0200

    Removed JSON.stringify.

--- Backend.js
+++ Backend.js
@@ -6,7 +6,7 @@ class Backend{
 			headers: {
 				Authentication: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2MTc3MCwiZXhwIjoxOTU5MjM3NzcwfQ.x_SvsnLkgYNEgpxa7h74Z__aBgGbGIYVmljhwYDJ1Bc'
 			},
-			body: JSON.stringify(data)
+			body: data
 		}).then(response => response.json());
 	}
 	static getBackend(){
