f148a065454cde6059bbda1e933840356d223b49
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2022-04-03 11:27:50 +0200

    Added Headers constructor.

--- Backend.js
+++ Backend.js
@@ -11,10 +11,10 @@ class Backend{
 	static getBackend(){
 		return localStorage.getItem('backend') ?? {
 			path: 'https://nfegdyrzrdhwvqpujxhj.functions.supabase.co',
-			headers: {
+			headers: new Headers({
 				'Content-Type': 'application/json',
-				Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2MTc3MCwiZXhwIjoxOTU5MjM3NzcwfQ.x_SvsnLkgYNEgpxa7h74Z__aBgGbGIYVmljhwYDJ1Bc'
-			}
+				'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2MTc3MCwiZXhwIjoxOTU5MjM3NzcwfQ.x_SvsnLkgYNEgpxa7h74Z__aBgGbGIYVmljhwYDJ1Bc'
+			})
 		};
 	}
 	static isOverride(){
