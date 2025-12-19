06dbc248706239e279a43b379368e12003857f62
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2025-12-19 08:13:19 +0100

    Switch from Router to HashRouter for improved routing; update routes to include a 404 component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
-import { Router, Route } from '@solidjs/router'
+import { HashRouter, Route } from '@solidjs/router'
 import './defaults.css'
 import App from './App.tsx'
 import Dev from './Dev.tsx'
 
-render(() => <Router>
-	<Route path="/" component={App} />
-	<Route path="/Dev" component={Dev} />
-</Router>, document.body!)
+render(() => <HashRouter>
+	<Route path="" component={App} />
+	<Route path="Dev" component={Dev} />
+	<Route path="*404" component={() => <div>404</div>} />
+</HashRouter>, document.body!)
