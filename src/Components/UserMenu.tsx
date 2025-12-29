b23529e081a9ec450046cc0f6cc278973b5c0c10
Christoffer Olofsson <15955091+ChrisAcrobat@users.noreply.github.com>
2025-12-29 08:13:30 +0100

    Refactor UserMenu component and update styles

--- src/Components/UserMenu.tsx
+++ src/Components/UserMenu.tsx
@@ -4,8 +4,7 @@ import styles from './UserMenu.module.css'
 import MenuItem from './MenuItem.tsx'
 
 export default function UserMenu() {
-	const isLoggedIn = GitHubService.isLoggedIn()
-	if (isLoggedIn) {
+	if (GitHubService.isLoggedIn()) {
 		GitHubService.fetch('user').then((response) => response.json()).then(async (user) => {
 			const sessionStorage = GitHubService.getSessionStorage() ?? {}
 			sessionStorage.username = user.login
@@ -16,20 +15,22 @@ export default function UserMenu() {
 
 	return (
 		<>
-			<Show when={isLoggedIn}>
+			<Show when={GitHubService.isLoggedIn()}>
 				<MenuItem>
 					<label class={styles.label}>
-						<div class={styles.logoutButton} onclick={() => GitHubService.logout()}>
-							Logout
+						<div class={styles.logoutButtonWrapper}>
+							&nbsp;
+							<div class={styles.logoutButton} onclick={() => GitHubService.logout()}>
+								Logout
+							</div>
 						</div>
-						&nbsp;
 						<span>{GitHubService.getSessionStorage().username}</span>
 					</label>
 					<img class={styles.currentProfileImage} alt='Current user profile image' src={GitHubService.getSessionStorage().avatar_url}></img>
 					<a href='/Dev'>Arena setups</a>
 				</MenuItem>
 			</Show>
-			<Show when={!isLoggedIn}>
+			<Show when={!GitHubService.isLoggedIn()}>
 				<div>
 					<a href={'https://algorithmic.games/login?origin=' + encodeURI(location.protocol + '//' + location.host + location.pathname)}>Login</a>
 				</div>
